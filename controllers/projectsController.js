2ea6af5f60ec3a3614bca10760e2f6d14f6469cb
debuggerdev24 <debuggerdev.dds@gmail.com>
2025-01-09 18:02:57 +0530

    added update and delete apis

--- controllers/projectsController.js
+++ controllers/projectsController.js
@@ -56,3 +56,81 @@ exports.getProjects = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
+
+exports.updateProject = async (req, res) => {
+  const { ProjectID } = req.params;
+  const updatedData = ({
+    Client_Name,
+    OrderID,
+    Address,
+    ProjectID,
+    Delivery_Status,
+    Payment_Status,
+    Project_Payment,
+    Photos,
+    Videos,
+    External_Media,
+    Property_Website,
+  } = req.body);
+
+  try {
+    const { data: existingProject, error: selectError } = await supabase
+      .from("Media_Projects")
+      .select("*")
+      .eq("ProjectID", ProjectID)
+      .single();
+
+    if (selectError || !existingProject) {
+      return res.status(404).json({ error: "Project not found" });
+    }
+
+    const { data, error } = await supabase
+      .from("Media_Projects")
+      .update(updatedData)
+      .eq("ProjectID", ProjectID)
+      .select();
+
+    if (error) {
+      return res.status(500).json({ error: error.message });
+    }
+
+    res.status(200).json({
+      message: "Project updated successfully",
+      data: data,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.deleteProject = async (req, res) => {
+  const { ProjectID } = req.params;
+
+  try {
+    const { data: existingProject, error: selectError } = await supabase
+      .from("Media_Projects")
+      .select("*")
+      .eq("ProjectID", ProjectID)
+      .single();
+
+    if (selectError || !existingProject) {
+      return res.status(404).json({ error: "Project not found" });
+    }
+
+    const { data, error } = await supabase
+      .from("Media_Projects")
+      .delete()
+      .eq("ProjectID", ProjectID);
+
+    if (error) {
+      return res.status(500).json({ error: error.message });
+    }
+
+    res.status(200).json({
+      message: "Project deleted successfully",
+      data: existingProject,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
--- routes/projects.js
+++ routes/projects.js
@@ -5,4 +5,6 @@ const authenticate = require("../middleware/authenticate");
 // Add a new product
 router.post("/", projectController.addProject);
 router.get("/", projectController.getProjects);
+router.put("/:ProjectID", projectController.updateProject);
+router.delete("/:ProjectID", projectController.deleteProject);
 module.exports = router;
